068f270a04d0c578b5eb69222b6e7bea95975600
Måns Abrahamsson <mans.abrahamsson@gmail.com>
2023-02-15 19:42:45 +0100

    adv16

--- labb_advent16_2.ex
+++ labb_advent16_2.ex
@@ -2,19 +2,11 @@ defmodule Day16 do
 
   def task(t) do
     start = :AA
-    rows = File.stream!("advent16_input.txt")
-    #rows = sample()
-    parsed_map = get_graph_map(parse(rows))
-
-    closed_taps = Map.new(Enum.filter(parsed_map,
-      fn(x) ->
-        {_, {rel, _}} = x
-        rel != 0
-      end
-    ))
-
-    #parsed
-    best = optimize_path(:AA, [], parsed_map, t, closed_taps, %{})
+    #rows = File.stream!("day16.csv")
+    rows = sample()
+    parsed = parse(rows)
+    parsed
+    best = optimize_path(:AA, [], parsed, t, 0, %{})
     {moves, score, _} = best
     {score, moves}
   end
@@ -28,89 +20,93 @@ defmodule Day16 do
 
   # Måste retunera det uppbyggda cachen.
 
-
-  def search_path(_, moves, graph, 0, _, cache) do
-    {[], 0, cache}
+  def optimize_path(_, moves, graph, 0, relesed_presure, cache) do
+    # Add released pressure from open taps
+    open_taps = get_open_taps(moves)
+    # Add presure of open taps
+    total_realeased = relesed_presure + total_pressure_release(open_taps, graph)
+    {[], total_realeased, cache}
   end
 
-  def search_path(curr, moves, graph, time_left, closed_taps, cache) do
+  # Två typer av moves:
+
+  #   {:on, kran_namn} # Kran_namn sätts på
+  #   {:move, kran_namn} # gå till kranamn
+
+
+  def optimize_path(curr, moves, graph, time_left, relesed_pressure, cache) do
+
     # Testa alla möjligheter och välj den bästa!
     legal = get_legal_moves(curr, graph)
+
 
     taps_on = get_open_taps(moves)
 
     # Beräkna hur mycket tryck som släpptes i senaste minuten
-    rate = total_pressure_release(taps_on, graph)
 
-    # Försök bara när det är nödvändigt
-    turn_on_tap_move =
-    if(Map.has_key?(closed_taps, curr)) do
-      optimize_path(curr, [{:on, curr}|moves], graph, time_left-1, Map.put(closed_taps, curr, curr), cache)
-    else
-      {[], 0, cache}
-    end
+    rate = total_pressure_release(taps_on, graph)
+    total = relesed_pressure + rate
+    # Låt den inte sätta på om kranen redan är påsatt.
+    turn_on_tap_move = optimize_path(curr, [{:on, curr}|moves], graph, time_left-1, total, cache)
     {_, _, new_cache} = turn_on_tap_move
+
 
 
     # Testa att gå alla andra möjliga vägar
     best_move = Enum.reduce(legal, {[], nil, -10, new_cache},
       fn(kran, acc) ->
         {_, _, _, new_cache} = acc
-        {best_moves, value, new_cache} = optimize_path(kran, [{:move, kran}|moves], graph, time_left-1, closed_taps, new_cache)
+        # is_tap_on = if(is_tap_on(kran, taps_on)) do :on else :off end
+        # Kolla om vägen redan finns i cachen
+        cached_result = new_cache[{kran, time_left, taps_on}]
 
-        # Add cache entry to new cache
-        # cache_self_release = value-((time_left-1)*rate)
-        new_cache = Map.put(new_cache, {kran, time_left-1, taps_on}, {best_moves, value})
-        new_acc = {best_moves, kran, value + rate, new_cache} # FEL? OSÄKER på +rate
+        IO.puts("-------------------")
+        IO.puts("Curr: #{curr}, DEAPTH: #{time_left}")
+        IO.inspect(cache)
+        IO.inspect(taps_on)
 
+        new_acc =
+        if(cached_result != nil) do
+          # IO.puts("+")
+          {best_moves, value} = cached_result
+          total_release_if_taken = value + (time_left)*rate # ska det vara -1?
+          {best_moves, kran, value, new_cache}
+        else
+          # IO.puts("-")
+          {best_moves, value, new_cache} = optimize_path(kran, [{:move, kran}|moves], graph, time_left-1, total, new_cache)
+          # Add cache entry to new cache
+          cache_self_release = value-(time_left*rate)
+          new_cache = Map.put(new_cache, {kran, time_left-1, taps_on}, {best_moves, cache_self_release})
+          {best_moves, kran, value, new_cache}
+        end
+        #IO.inspect(new_acc)
         {_,_, new_value, new_cache} = new_acc
-
         # Update acc if better
         {a_moves, a_kran, a_max, _} = acc
         if(a_max<new_value) do
           new_acc
         else
-          #Update the cache of acc
-          {a_moves, a_kran, a_max, new_cache}
+          #Update the cache
+          {a_moves, a_kran, a_max,  new_cache}
         end
       end
     )
 
+
+
     {_, _, best_move_realeased, new_cache} = best_move
     {_, turn_on_released, _} = turn_on_tap_move
-    turn_on_released = turn_on_released + rate
+
 
     # LÄGG ÄVEN TILL DET NYA MOVET!!!
     if(best_move_realeased >= turn_on_released) do
       {best_future_moves, kran, value, _} = best_move
-      {[{:move, kran} | best_future_moves], best_move_realeased, new_cache}
+      {[{:move, kran} | best_future_moves], value, new_cache}
     else
-      {best_future_moves, _, _} = turn_on_tap_move
-      # Lägg till i cachen
-      # new_cache = Map.put(new_cache, {curr, time_left, taps_on}, {best_future_moves, turn_on_released})
-
-      {[{:on,   curr} | best_future_moves], turn_on_released, new_cache} # måste adda rate här!
+      {best_future_moves, value, _} = turn_on_tap_move
+      {[{:on,   curr} | best_future_moves], value, new_cache}
     end
-  end
 
-
-  # Två typer av moves:
-
-  #   {:on,   kran_namn} # Kran_namn sätts på
-  #   {:move, kran_namn} # gå till kran_namn
-
-
-  def optimize_path(curr, moves, graph, time_left, closed_taps, cache) do
-    open_taps = get_open_taps(moves)
-    cached_result = cache[{curr, time_left, open_taps}]
-
-    if(cached_result != nil) do
-
-      {best_moves, released} = cached_result
-      {best_moves, released, cache} # Rätt? är + rate rätt?
-    else
-      search_path(curr, moves, graph, time_left, closed_taps, cache)
-    end
   end
 
   def total_pressure_release(open_taps, graph) do
@@ -138,15 +134,6 @@ defmodule Day16 do
     taps_on
   end
 
-  def get_graph_map(graph_list) do
-    Enum.reduce(graph_list, %{},
-      fn(line, acc) ->
-        {k, v} = line
-        Map.put(acc, k, v)
-      end
-    )
-  end
-
   def is_in_map(_, []) do false end
   def is_in_map(key, map) do
     Enum.reduce(map, false,
@@ -160,7 +147,6 @@ defmodule Day16 do
     Map.has_key?(taps, tap)
   end
 
-  # Lookup är långsam, använd en map istället för en lista med linjär sökning!!!
   def lookup(_, []) do [] end
   def lookup(key, map) do
     filterd = Enum.filter(map, fn({x,_}) -> x==key end)
@@ -173,10 +159,13 @@ defmodule Day16 do
     end
   end
 
-  def get_legal_moves(curr, graph) do
-    case graph[curr] do
-      nil -> []
-      {_, list} -> list
+  def get_legal_moves(curr, moves) do
+    tups = lookup(curr, moves)
+    if(tups==nil) do
+      []
+    else
+      {_, list} = tups
+      list
     end
   end
 
@@ -203,7 +192,7 @@ defmodule Day16 do
 
   def sample() do
     # OBS FLOW RATE AV AA = 0 från början!!!
-    ["Valve AA has flow rate=0; tunnels lead to valves DD, II, BB",
+    ["Valve AA has flow rate=1; tunnels lead to valves DD, II, BB",
      "Valve BB has flow rate=13; tunnels lead to valves CC, AA",
      "Valve CC has flow rate=2; tunnels lead to valves DD, BB",
      "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE",
@@ -213,14 +202,6 @@ defmodule Day16 do
      "Valve HH has flow rate=22; tunnel leads to valve GG",
      "Valve II has flow rate=0; tunnels lead to valves AA, JJ",
      "Valve JJ has flow rate=21; tunnel leads to valve II"]
-    # [
-    #   #"Valve AA has flow rate=10; tunnels lead to valves AA, BB",
-    #   #"Valve BB has flow rate=20; tunnels lead to valves AA, BB",
-    #   "Valve AA has flow rate=10; tunnels lead to valves BB, CC, DD",
-    #   "Valve BB has flow rate=20; tunnels lead to valves AA, CC, DD",
-    #   "Valve CC has flow rate=30; tunnels lead to valves BB, AA, DD",
-    #   "Valve DD has flow rate=40; tunnels lead to valves BB, CC, AA",
-    # ]
   end
 
 
